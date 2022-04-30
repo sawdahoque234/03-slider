aea347dd6b8e8ba3209759d5132ab8fd27fe5ec1
sawdahoque234 <sawdahoque6066@gmail.com>
2022-04-30 23:21:10 +0600

    react-slider

---
+++ src/Slider/data.js
@@ -0,0 +1,40 @@
+const reviews = [
+    {
+      id: 1,
+      name: 'Mr.Smith',
+      title: 'web developer',
+      image:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRr3qBVX4XIA8zq3LpBn64zAuOt9_IZ7_H5uA&usqp=CAU',
+      text:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni dolore illo eveniet perferendis voluptatem tenetur quaerat iure possimus deserunt odit?'
+    },
+    {
+      id: 2,
+      name: 'Miss Jenny',
+      title: 'web designer',
+      image:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDtNm2Q2NkCm8QQWLuzRiY9w5oDAdl1hWUT0cgZqg5hZDlY-7H-8VhMMSzY3g2NrkV6Co&usqp=CAU',
+      text:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni dolore illo eveniet perferendis voluptatem tenetur quaerat iure possimus deserunt odit?'
+    },
+    {
+      id: 3,
+      name: 'Peter jones',
+      title: 'Project Manager',
+      image:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSv9zzOmzF32TNcQ2O93T21Serg2aJj5O-1hrQdZiE6ITGiKLsW4rjgVpX-asQYXa4iVeA&usqp=CAU',
+      text:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni dolore illo eveniet perferendis voluptatem tenetur quaerat iure possimus deserunt odit?',
+    },
+    {
+      id: 4,
+      name: 'Miss Liliy',
+      title: 'the boss',
+      image:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREiCJ798vZGXp83MAI9tCkpg8AM7r1ubbqPn8sTEWG9wL-AmxOZivjR8IpaYz5qYpZinQ&usqp=CAU',
+      text:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni dolore illo eveniet perferendis voluptatem tenetur quaerat iure possimus deserunt odit?'
+    },
+  ];
+  
+  export default reviews;
